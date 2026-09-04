74b451c37ee8824bb0310602cde6503cc23b2364
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-04 21:11:56 +0530

    adding a basic program how to print employee details

---
+++ Basics/basic-programs/Employee.java
@@ -0,0 +1,10 @@
+
+public class Employee {
+
+        public static void main(String[] args) {
+            System.out.println("Employee Name : Mani");
+            System.out.println("Employee id : 160");
+            System.out.println("Employee Salary : 20000");
+        }
+    
+}
