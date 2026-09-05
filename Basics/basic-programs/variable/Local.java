1b593c178edfb89a1911e0a9a066ffc28590692a
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-05 05:48:27 +0530

    example rpograms for typesof variables

---
+++ Basics/basic-programs/variable/Local.java
@@ -0,0 +1,19 @@
+
+public class Local {
+    public static void main(String[] args) {
+        int a = 36;
+        long b = 123456789;
+        float c = 2.556f;
+        double d = 55.264d;
+        char e = 'm';
+        String f = "poiuytre";
+        System.out.println("Hello World");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+
+}
