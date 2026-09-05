f2791470b1e39d42c125d5317c10356f0f1fb3f3
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-05 11:04:55 +0530

    Add unary operator program

---
+++ Basics/operators/unaryoperators/Program1.java
@@ -0,0 +1,13 @@
+public class Program1 {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a++ + a-- + --a + a++ - --a + a-- + ++a - a);
+    }
+}
+/*
+ * unary operator - operator which accepts one operand as a parameter
+ * thos operand are knownas unary operator
+ * two types
+ * -->Increment-PostIncrement(a++) and PreIncrement(++a)
+ * -->Decrement-PostDecrement(a++) and PreDecrement(++a)
+ */
