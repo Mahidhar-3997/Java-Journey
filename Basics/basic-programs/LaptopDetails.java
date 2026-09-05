5c8ed414bce9e88a485f5782be7e829261cf34d3
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-05 05:58:55 +0530

    programs

---
+++ Basics/basic-programs/LaptopDetails.java
@@ -0,0 +1,12 @@
+public class LaptopDetails {
+    public static void main(String[] args) {
+        System.out.println("Detais of Laptop");
+        System.out.println("Laptop Name : Dell");
+        System.out.println("Laptop Size : 15.6");
+        System.out.println("Processor : Ryzen");
+        System.out.println("Colour : Black");
+        System.out.println("Ram : 8gb");
+        System.out.println("Rom : 1tb");
+        System.out.println("Graphic card : radeon");
+    }
+}
