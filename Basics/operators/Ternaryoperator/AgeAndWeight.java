2db4974253c948258c77b83ff9813def269892f5
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-06 21:39:48 +0530

    Remove Scanner class documentation comments
    Removed comments explaining the Scanner class and its methods.

--- Basics/operators/Ternaryoperator/AgeAndWeight.java
+++ Basics/operators/Ternaryoperator/AgeAndWeight.java
@@ -11,29 +11,3 @@ public class AgeAndWeight {
 		System.out.println("result : " + result);
 	}
 }
-/*
- * Scanner class:
- *
- * Scanner is a predefined class in Java used to read input from the user.
- * It is available in the java.util package.
- *
- * To use Scanner, we need to import it:
- * import java.util.Scanner;
- *
- * Scanner can read different types of input such as:
- * int, float, double, long, String, char, etc.
- *
- * Common Scanner methods:
- * nextInt()    -> reads an integer
- * nextFloat()  -> reads a float
- * nextDouble() -> reads a double
- * nextLong()   -> reads a long value
- * next()       -> reads a single word
- * nextLine()   -> reads a complete line
- *
- * Example:
- * Scanner sc = new Scanner(System.in);
- * int age = sc.nextInt();
- *
- * System.in is used to receive input from the keyboard.
- */
