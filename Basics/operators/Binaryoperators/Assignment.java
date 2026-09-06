ab784b5c62ebacb0f59709cbf985f40b956a6fc3
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-06 21:02:32 +0530

    program for assignment operator

---
+++ Basics/operators/Binaryoperators/Assignment.java
@@ -0,0 +1,27 @@
+public class Assignment {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println("a = " + a);
+
+        a += 5;
+        System.out.println("a += 5 : " + a);
+
+        a -= 3;
+        System.out.println("a -= 3 : " + a);
+
+        a *= 2;
+        System.out.println("a *= 2 : " + a);
+
+        a /= 4;
+        System.out.println("a /= 4 : " + a);
+
+        a %= 3;
+        System.out.println("a %= 3 : " + a);
+    }
+}
+/*
+Assignment operators are (+=,-=,*=,/=,%=)
+a = a+b is represented as a+=b
+*/
