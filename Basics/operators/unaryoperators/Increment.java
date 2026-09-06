0f6bf4197dc53604d7be556988d8cc276adcdb48
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-06 21:18:50 +0530

    program for increment operator

---
+++ Basics/operators/unaryoperators/Increment.java
@@ -0,0 +1,14 @@
+public class Increment {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        // Post-increment
+        System.out.println("Post-increment : " + a++);
+        System.out.println("After post-increment : " + a);
+
+        // Pre-increment
+        System.out.println("Pre-increment : " + ++a);
+        System.out.println("After pre-increment : " + a);
+    }
+}
