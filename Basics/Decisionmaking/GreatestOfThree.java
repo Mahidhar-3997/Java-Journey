4cefc29287c832e5f4c4a07d993baae6dbbaf8f7
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-08 16:07:17 +0530

    adding a progrom to find which greates number

---
+++ Basics/Decisionmaking/GreatestOfThree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    public static void main(String[] args) {
+        System.out.println("Enter the values");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a > b && a > c) {
+            System.out.println("a is greater number");
+        } else if (b > a && b > c) {
+            System.out.println("b is greater number");
+        } else {
+            System.out.println("c is greater number");
+        }
+    }
+}
