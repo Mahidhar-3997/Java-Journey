7b26fb5b99729cd621d71d1ce26c9ff3bf9e70ce
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-10 22:15:00 +0530

    program to find sum of numbers

---
+++ Basics/loops/forloop/SumOfNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("sum of number :" + sum);
+        sc.close();
+    }
+}
