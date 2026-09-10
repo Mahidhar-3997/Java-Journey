5b6c1790e4fbcae3fc0b5e9499eab4151183eaa8
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-10 22:09:06 +0530

    program to find sum of numbers

---
+++ Basics/loops/whileloop/SumOfNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("sum of natural number " + sum);
+        sc.close();
+
+    }
+}
