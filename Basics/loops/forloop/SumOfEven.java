0742d07cc1b0bbfa763a972f79c430eebe0f5b28
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-11 05:51:35 +0530

    program for loops

--- Basics/loops/forloop/SumOfEven.java
+++ Basics/loops/forloop/SumOfEven.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class SumOfEven {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number :");
         int n = sc.nextInt();
