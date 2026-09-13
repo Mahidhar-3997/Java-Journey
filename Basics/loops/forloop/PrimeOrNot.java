fcf91f0770e94318a2f769200deb2951c8fba008
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-13 13:53:28 +0530

    program to check prime number or not

---
+++ Basics/loops/forloop/PrimeOrNot.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        int factCount = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                factCount++;
+            }
+        }
+        if (factCount == 2) {
+            System.out.println("Prime number");
+
+        } else {
+            System.out.println("Not a prime number");
+        }
+        sc.close();
+
+    }
+}
