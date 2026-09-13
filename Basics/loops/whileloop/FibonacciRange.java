813ba1ccfe6899f85c5a512e969a79b38bfb4a77
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-13 14:33:29 +0530

    peogram to print fibonacci series in range

---
+++ Basics/loops/whileloop/FibonacciRange.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FibonacciRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        System.out.print(a + " " + b + " " + c + " ");
+        for (;;) {
+            int d = a + b + c;
+            if (d > n) {
+                break;
+            }
+            System.out.print(d + " ");
+            a = b;
+            b = c;
+            c = d;
+        }
+        sc.close();
+
+    }
+}
