e1fc487849824dde4c9aa8cd39b8706991317fea
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-26 08:06:07 +0530

    second largest prime num in range

---
+++ numberprograms/SecondLargestPrime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SecondLargestPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range :");
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int count = 0;
+        for (int n = end; n >= start; n--) {
+            if (n <= 1) {
+                continue;
+            }
+            boolean flag = true;
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                count++;
+                if (count == 2) {
+                    System.out.println(n);
+                }
+            }
+        }
+        sc.close();
+    }
+}
