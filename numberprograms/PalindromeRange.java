b0c9cc5ebd2980a18976c0037ab308a64799a96e
Mahidhar-3997 <mahidharmaddasani@gmail.com>
2026-09-26 08:02:45 +0530

    program to pirnt palindrome num in range

---
+++ numberprograms/PalindromeRange.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range :");
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        for (int n = start; n <= end; n++) {
+            int temp = n;
+            int rev = 0;
+            while (temp > 0) {
+                int rem = temp % 10;
+                rev = rev * 10 + rem;
+                temp /= 10;
+            }
+            if (rev == n) {
+                System.out.println(n);
+            }
+        }
+        sc.close();
+
+    }
+}
